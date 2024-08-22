323cda3658117d4c58d597908365ab5dcb939d2e
Jordan Kay <jordan@fleuronic.com>
2024-08-22 12:04:17 -0400

    Use Swift 5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import CompilerPluginSupport
@@ -47,6 +47,5 @@ let package = Package(
                 .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
             ]
         ),
-    ],
-	swiftLanguageVersions: [.v6]
+    ]
 )
